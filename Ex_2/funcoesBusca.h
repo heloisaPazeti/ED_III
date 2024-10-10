b789d6efe69585c0bdf45105282bb56ba969e603
heloisaPazeti <helopazeti@gmail.comm>
2024-10-10 11:03:43 +0000

    Adding new files and removing old

---
+++ Ex_2/funcoesBusca.h
@@ -0,0 +1,18 @@
+#ifndef FUNCOES_BUSCA_H
+#define FUNCOES_BUSCA_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "structs.h"
+
+RegDados lerRegistro(FILE *arquivo);
+int definirTipo(char *nomeCampo);
+void imprimirRegistro(RegDados registro);
+int numPagDisco(FILE *arquivo);
+void eliminarRegistro(FILE* arquivo, int encadeamento);
+int buscarRRN(FILE *arquivo);
+
+#endif
+
+
