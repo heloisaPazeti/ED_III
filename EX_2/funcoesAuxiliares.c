7ef3044bd3940acf7cc522aaec4c1815708c001f
heloisaPazeti <helopazeti@gmail.comm>
2024-10-21 15:01:30 +0000

    a2

--- EX_2/funcoesAuxiliares.c
+++ EX_2/funcoesAuxiliares.c
@@ -53,12 +53,22 @@ int EscreveNo(char *nomeArq, NoArvBin no, int rrn)
     int i;
     long int offset = (rrn*tamNo) + tamCabecalhoArvore;
     FILE *arqArvBin;
-    arqArvBin = fopen(nomeArq, "wb");
-    if(ChecarIntegridadeArquivo(arqArvBin, nomeArq) == -1) return -1;
+    arqArvBin = fopen(nomeArq, "rb+");
+    //if(ChecarIntegridadeArquivo(arqArvBin, nomeArq) == -1) return -1;
 
-    fclose(arqArvBin);
-    CabecalhoArvBin cabecalho = LerCabecalhoArvore(nomeArq);
-    printf("NO RAIZ: %d\n", cabecalho.noRaiz);
+    //fclose(arqArvBin);
+    //CabecalhoArvBin cabecalho = LerCabecalhoArvore(nomeArq);
+    //printf("NO RAIZ: %d\n", cabecalho.noRaiz);
+
+    fseek(arqArvBin, 0, SEEK_SET);
+
+    char status;
+    int raiz;
+
+    fread(&status, sizeof(char), 1, arqArvBin);
+    fread(&raiz, sizeof(int), 1, arqArvBin);
+
+    printf("RAIZ: %d\n", raiz);
 
     fseek(arqArvBin, offset, SEEK_SET);
     printf("OFFSET: %ld || CURRENT POSITION: %ld\n", offset, ftell(arqArvBin));
